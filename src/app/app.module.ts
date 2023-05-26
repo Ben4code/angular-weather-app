d3602e8c2373af2e6e8a3f99058316cf0b5f9c39
ben4code <nnaemeka.obioha@gmail.com>
2023-05-26 16:42:47 -0400

    added route and base css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherDetailComponent } from './components/weather-detail/weather-detail.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WeatherDetailComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
