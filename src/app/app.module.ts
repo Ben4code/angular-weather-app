0a5a8529d58d6c4166047814d4f8c3e39789c7fa
ben4code <nnaemeka.obioha@gmail.com>
2023-05-26 15:31:27 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherDetailComponent } from './components/weather-detail/weather-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherDetailComponent,
   ],
   imports: [
     BrowserModule,
