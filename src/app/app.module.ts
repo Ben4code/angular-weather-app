c5bb4fff588e2612e0b43c86cca93eff766069f9
ben4code <nnaemeka.obioha@gmail.com>
2023-05-26 23:47:57 -0400

    added landing ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherDetailComponent } from './components/weather-detail/weather-detail.component';
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WeatherDetailComponent,
     HomeComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
